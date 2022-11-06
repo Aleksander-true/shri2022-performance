aa1359088c2ecc534e302398e154f8b8421525d2
Aleksander-true <aleksander-true@yandex.ru>
2022-11-06 14:00:06 +0300

    roll back

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-//import "./fonts.css";
-//import "./reset.css";
-//import "./styles.css";
-//import "./vendors/bootstrap.css";
+import "./fonts.css";
+import "./reset.css";
+import "./styles.css";
 
 import "./scripts.js";
